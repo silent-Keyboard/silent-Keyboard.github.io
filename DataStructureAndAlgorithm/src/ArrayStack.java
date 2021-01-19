18b5de1eef62f20f6f657bef4e0d0b48cb5d7e8a
GC朱文豪 <zhuwenhao@gcsoft.com>
2021-01-19 17:38:04 +0800

    no message

--- DataStructureAndAlgorithm/src/ArrayStack.java
+++ DataStructureAndAlgorithm/src/ArrayStack.java
@@ -3,68 +3,83 @@
 // (powered by FernFlower decompiler)
 //
 
+import java.util.Map;
+
 public class ArrayStack {
-    private String[] arrayStack;
-    private int size;
-    private int count;
+    private Object[] arrayStack;
+    private int top;
 
-    public ArrayStack() {
+    public ArrayStack( int MAXSIZE) {
+        this.arrayStack = new Object[MAXSIZE];
+        this.top = 0;
     }
 
-    public void initStack(int n) {
-        this.arrayStack = new String[n];
-        this.size = n;
-        this.count = 0;
+    public boolean isEmpty() {
+        return this.top == 0 ? true : false;
+    }
+    public void push(Object element) {
+        this.arrayStack[top] = element;
+        top = top + 1;
     }
 
-    public void push(String element) {
-        if (this.count >= this.size) {
-            String[] temp = new String[2 * this.size];
-
-            for(int i = 0; i < this.size; ++i) {
-                temp[i] = this.arrayStack[i];
-            }
-
-            this.size = 2 * this.size;
-            this.arrayStack = temp;
-        }
-
-        this.arrayStack[this.count] = element;
-        ++this.count;
-    }
-
-    public String pop() throws MyException {
-        String str = "";
-        if (this.count > 0) {
-            str = this.arrayStack[this.count - 1];
-            --this.count;
-            return str;
+    public Object pop() throws MyException{
+        if (!isEmpty()) {
+            this.top = this.top - 1;
+            return this.arrayStack[top];
         } else {
-            throw new MyException("The Stack is empty!");
+            throw new MyException("The Stack is Empty!");
         }
     }
 
+    public Object peek() throws MyException{
+        if (!isEmpty()) {
+            return this.arrayStack[top -1];
+        } else {
+            throw new MyException("The Stack is Empty!");
+        }
+
+    }
     public void output() {
-        for(int i = 0; i < this.count; ++i) {
+        for(int i = 0; i < this.top; ++i) {
             System.out.println(this.arrayStack[i]);
         }
+    }
 
+    // Four simple operations: + - * /
+    public static void calculateExpression(ArrayStack number, ArrayStack operator, String exp) {
+        String[] numbers = exp.split("\\+|-|\\*|/");
+
+        for (int i = 0; i < numbers.length; i++) {
+            number.push(numbers[i]);
+            System.out.println(numbers[i]);
+        }
+
+        for (int i = 0; i < exp.length(); i++) {
+            if ('+' == (exp.charAt(i)) || '-' == (exp.charAt(i)) || '*' == (exp.charAt(i)) || '/' == (exp.charAt(i))) {
+                operator.push(exp.substring(i, i+1));
+                System.out.println(exp.substring(i, i+1));
+            }
+        }
+
+        char[] cur = exp.toCharArray();
+        for (int i = 0; i < cur.length; i++) {
+            if (Character.isDigit(cur[i])) {
+                StringBuffer str = new StringBuffer();
+                while(i < cur.length && Character.isDigit(cur[i])) {
+                    str.append(cur[i]);
+                    i = i + 1;
+                }
+                i = i -1;
+                number.push(Integer.parseInt(str.toString()));
+            }
+        }
     }
 
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack();
-        stack.initStack(10);
-
-        for(int i = 0; i < 12; ++i) {
-            stack.push(i + "dolar");
-        }
-
-        try {
-            stack.pop();
-        } catch (MyException var3) {
-            var3.printStackTrace();
-        }
-
-        stack.output();
+        // 泛型栈 数据转型
+        ArrayStack stack1 = new ArrayStack(10);
+        ArrayStack stack2 = new ArrayStack(10);
+        ArrayStack cur = new ArrayStack(10);
+        calculateExpression(stack1, stack2,  "123+6*7-10/2");
     }
 }
